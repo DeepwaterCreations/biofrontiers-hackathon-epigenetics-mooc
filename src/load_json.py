37583e7a25e34c77cc01fe51052bf7a32bd4217f
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2017-05-22 16:28:31 -0600

    Combine if statements for cleaner cleaning code

--- src/load_json.py
+++ src/load_json.py
@@ -13,24 +13,25 @@ def clean_data(data):
     #Iterate over student ids
     for student_id in data.keys():
         scores = []
-        if 'answers' not in data[student_id] or data[student_id]['answers'] == {}:
+        if 'answers' not in data[student_id] or \
+            'evaluations' not in data[student_id] or \
+            data[student_id]['answers'] == {}:
             continue
         #For each id, iterate over the reviews
-        if 'evaluations' in data[student_id]:
-            for reviewer in data[student_id]['evaluations'].values():
-                #For each review, slice off the comments
-                reviewer_scores = reviewer[:17]
-                #Also, divide into subsets matched to questions
-                reviewer_scores = [reviewer_scores[:7], reviewer_scores[7:11], reviewer_scores[11:14], reviewer_scores[14:]]
-                #Turn strings into ints
-                for q in range(len(reviewer_scores)):
-                    reviewer_scores[q] = map(int, reviewer_scores[q])
-                #Average the new scores into the existing list
-                if scores == []:
-                    scores = reviewer_scores
-                else:
-                    for q in range(len(scores)):
-                        scores[q] = [np.mean(list(s)) for s in zip(scores[q], reviewer_scores[q])]
+        for reviewer in data[student_id]['evaluations'].values():
+            #For each review, slice off the comments
+            reviewer_scores = reviewer[:17]
+            #Also, divide into subsets matched to questions
+            reviewer_scores = [reviewer_scores[:7], reviewer_scores[7:11], reviewer_scores[11:14], reviewer_scores[14:]]
+            #Turn strings into ints
+            for q in range(len(reviewer_scores)):
+                reviewer_scores[q] = map(int, reviewer_scores[q])
+            #Average the new scores into the existing list
+            if scores == []:
+                scores = reviewer_scores
+            else:
+                for q in range(len(scores)):
+                    scores[q] = [np.mean(list(s)) for s in zip(scores[q], reviewer_scores[q])]
 
         print("ID: {0} scores: {1}".format(student_id, scores))
     #Extract answers and match with combined subsets
