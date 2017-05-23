4fd3ed07c589bded700cc188deba3fc37435e740
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2017-05-23 09:37:15 -0600

    Combine averaged scores with associated student answers

--- src/load_json.py
+++ src/load_json.py
@@ -10,6 +10,12 @@ def load_data(data_filename):
     return data
 
 def clean_data(data):
+    """Strip garbage from data and reorganize
+
+    Output: A list of sublists, where each sublist is a student's answer to one question followed by
+    the averages of the scores given to that question by the student's reviewers.
+    """
+    cleaned_data = []
     #Iterate over student ids
     for student_id in data.keys():
         scores = []
@@ -25,7 +31,7 @@ def clean_data(data):
             reviewer_scores = [reviewer_scores[:7], reviewer_scores[7:11], reviewer_scores[11:14], reviewer_scores[14:]]
             #Turn strings into ints
             for q in range(len(reviewer_scores)):
-                reviewer_scores[q] = map(int, reviewer_scores[q])
+                reviewer_scores[q] = list(map(int, reviewer_scores[q]))
             #Average the new scores into the existing list
             if scores == []:
                 scores = reviewer_scores
@@ -33,9 +39,13 @@ def clean_data(data):
                 for q in range(len(scores)):
                     scores[q] = [np.mean(list(s)) for s in zip(scores[q], reviewer_scores[q])]
 
-        print("ID: {0} scores: {1}".format(student_id, scores))
-    #Extract answers and match with combined subsets
-    #TODO
+        #Extract answers and match with combined subsets
+        answers = data[student_id]['answers']
+        for i, q in enumerate(['Q1', 'Q2', 'Q3', 'Q4']):
+            if q in answers:
+                cleaned_data.append([answers[q]] + scores[i])
+            
+    return cleaned_data
 
 
 if __name__ == "__main__":
