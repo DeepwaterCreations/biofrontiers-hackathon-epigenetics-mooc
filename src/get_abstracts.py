88ee023edceeb9687f893aed0ae6801cad1ddbf0
Deepwater Creations <Dylan.Craine@DeepwaterCreations.com>
2017-05-24 14:28:21 -0600

    Read abstracts from multiple xml files in parallel

--- src/get_abstracts.py
+++ src/get_abstracts.py
@@ -1,12 +1,37 @@
 #!/usr/bin/env python3
-
 import sys
+import os
+import glob
 import xml.etree.ElementTree as ET
+from threading import Thread
+
+class XMLReadingThread(Thread):
+    def __init__(self, filename):
+        super(XMLReadingThread, self).__init__()
+        self.filename = filename
+        self.results = []
+
+    def run(self):
+        tree = ET.parse(self.filename)
+        abstracts = tree.getroot().findall(".//AbstractText")
+        abs = []
+        for abstract in abstracts:
+            for text in abstract.itertext():
+                abs.append(text)
+        self.results = abs
 
 if __name__ == "__main__":
-    filename = sys.argv[1]
-    tree = ET.parse(filename)
-    abstracts = tree.getroot().findall(".//AbstractText")
-    for abstract in abstracts:
-        for text in abstract.itertext():
-            print(text)
+    filedesc = sys.argv[1]
+    filelist = glob.glob(filedesc)
+    threads = []
+    for xml_file in filelist:
+        t = XMLReadingThread(filename=xml_file)
+        t.start()
+        threads.append(t)
+
+    full_results = []
+    for t in threads:
+        t.join()
+        results = "\n".join(t.results)
+        full_results.append(results)
+    print("\n".join(full_results))
